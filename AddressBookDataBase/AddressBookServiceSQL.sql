362fb148a3883488841ae3e0e6f377829e494cc7
satyamshete <shetesatyam99@gmail.com>
2022-09-25 22:09:35 +0530

    [Satyam] Filter Data from Table

--- AddressBookDataBase/AddressBookServiceSQL.sql
+++ AddressBookDataBase/AddressBookServiceSQL.sql
@@ -19,8 +19,13 @@ values('Karthik','Goswami','Near Delishop','Junagadh','MadhyaPradesh',4125634,56
 ('Abhijeet','Raina','Shivneri colony','Mumbai','Marashtra',410001,2564565515,'Abhijeet@gmail.com')
 -------------UC4-----------
 update AddressBook Set Email = 'Abhi@Gmail.com' where FirstName = 'Abhijeet'
+update AddressBook Set State = 'Maharashtra' where FirstName = 'Abhijeet'
 ------------UC5-----------
 delete from AddressBook where FirstName='Karthik'
 ------------UC6----------
 select * from AddressBook where City='Mumbai' or State='Maharashtra'
-
+-----------UC7---------------
+insert into AddressBook
+values('Monika','Ghargote','Krishna Temple','Dhule','Maharashtra',4127434,9812645897,'monika@gmail.com'),
+('Namitha','Gulhane','Westside','Kolkata','WestBangol',741256,2515256284,'Namitha@gmail.com')
+select count(FirstName) from AddressBook where City='Mumbai' And State='Maharashtra'
