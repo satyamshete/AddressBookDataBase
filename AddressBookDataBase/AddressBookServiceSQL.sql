3bdb85790356337c96fe8762c1f99dd3dca5f5f9
satyamshete <shetesatyam99@gmail.com>
2022-09-25 21:32:25 +0530

    [Satyam] Create Table AddressBook

--- AddressBookDataBase/AddressBookServiceSQL.sql
+++ AddressBookDataBase/AddressBookServiceSQL.sql
@@ -1,3 +1,15 @@
 ﻿---------------UC1---------------
 create database AddressBookService
 use AddressBookService
+--------------UC2------------
+create table AddressBook (
+FirstName varchar (30) primary key,
+LastName varchar (30), 
+Address varchar(40), 
+City varchar (10),
+State varchar(30), 
+Zip int,
+PhoneNumber bigint,
+Email varchar(30)
+);
+select * from AddressBook
